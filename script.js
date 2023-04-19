fb85243dd0687644b0b8c9cbebadb6426d3a8ad7
myaooooo98 <yaoer0505@gmail.com>
2023-04-19 17:17:38 +0800

    improve function to create diff grid size, additional features

--- script.js
+++ script.js
@@ -1,56 +1,79 @@
 const container = document.querySelector('.container');
 
-// !!!! INCOMPLETE !!!!
 // obtain the grid size
-// const gridRange = document.getElementById('gridSize');
-// let gridChange = false;
-let gridSize = 16;
+const gridRange = document.getElementById('gridSize');
 
-// function changeGridSize() {
-    
-// }
+// update the browser to show which grid size they choose
+function updateLabel(e) {
+    const gridLabel = document.querySelector('label[for="grid-size"]');
+    gridLabel.innerHTML = `${e.value} x ${e.value}`;
+}
 
-// gridRange.addEventListener('input', (e) => {
-//     gridChange = true;
-// });
+// set the initial grid size to 16 x 16
+createGrid(gridRange);
+updateLabel(gridRange);
 
+gridRange.addEventListener('mousemove', (e) => {
+    updateLabel(e.target);
+});
+
+gridRange.addEventListener('change', (e) => {
+    updateLabel(e.target);
+    createGrid(e.target);
+});
 
 // create the grid
-function createGrid(gridSize){
-    for (let i = 0; container.childElementCount < gridSize * gridSize; i++){
+function createGrid(e){
+    container.innerHTML = '';
+    for (let i = 0; container.childElementCount < e.value * e.value; i++){
         // create the div for grid
         let grid = document.createElement('div');
         grid.classList.add('grid');
 
         // set the height and width
-        grid.style.width = `${container.clientWidth / gridSize}px`;  // clientWidth = size of content, offsetWidth = size of content + border
-        grid.style.height = `${container.clientHeight / gridSize}px`;
+        grid.style.width = `${container.clientWidth / e.value}px`;  // clientWidth = size of content, offsetWidth = size of content + border
+        grid.style.height = `${container.clientHeight / e.value}px`;
 
         // insert to the div container
         container.appendChild(grid);
     }
+
+    // call the draw function
+    draw();
 }
 
-createGrid(gridSize);
 
 // when the grid is click, add color to it
-const grids = document.querySelectorAll('.grid');
-let isDrawing = false;
+function draw() {
+    const grids = document.querySelectorAll('.grid');
+    let isDrawing = false;
 
-grids.forEach(grid => {
-    // when mousedown, start to color the grid
-    grid.addEventListener('mousedown', (e) => {
-        isDrawing = true;
-        e.target.classList.add('colored');
-    });
-
-    // stop to color when mouse up
-    grid.addEventListener('mouseup', () => isDrawing = false);
-
-    // change the color of grid when mouse hover to the div and mousedown
-    grid.addEventListener('mousemove', (e) => {
-        if (isDrawing) {
+    grids.forEach(grid => {
+        // when mousedown, start to color the grid
+        grid.addEventListener('mousedown', (e) => {
+            isDrawing = true;
             e.target.classList.add('colored');
-        }
+        });
+    
+        // stop to color when mouse up
+        grid.addEventListener('mouseup', () => isDrawing = false);
+    
+        // change the color of grid when mouse hover to the div and mousedown
+        grid.addEventListener('mousemove', (e) => {
+            if (isDrawing) {
+                e.target.classList.add('colored');
+            }
+        });
     });
-});
+}
+
+
+// obtain the color (incomplete)
+// const colorChosen = document.querySelector('#color');
+// const coloredGrids = document.querySelectorAll('.colored');
+
+// colorChosen.addEventListener('change', (e) => {
+//     coloredGrids.forEach(colored => {
+//         colored.style.backgroundColor = e.target.value;
+//     });
+// });
